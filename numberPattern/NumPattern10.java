6f4400f63f18e578a0dad3164624c0ce8928947c
Ravi <raviranjanpandey15@gmail.com>
2021-10-17 22:54:04 +0530

    New Files

---
+++ numberPattern/NumPattern10.java
@@ -0,0 +1,20 @@
+/*
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15
+*/
+class NumPattern10{
+	public static void main(String[] args) {
+		int rows= 5;
+		int k=1;
+		for (int i=1; i<=rows ; i++ ) {
+			for (int j=1; j<=i ; j++ ) {
+				System.out.print(k+" ");
+				k++;
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ numberPattern/NumPatterrn7.java
@@ -0,0 +1,22 @@
+/*
+5 4 3 2 1 
+5 4 3 2 
+5 4 3 
+5 4 
+5
+*/
+
+class  NumPatterrn7{
+	public static void main(String[] args) {
+		int rows= 5;
+		for (int i=1; i<=rows ; i++ ) {
+			for (int j=rows; j>=i ; j-- ) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+	
+
+}
